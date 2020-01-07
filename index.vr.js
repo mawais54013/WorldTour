63bcdf84b25e8239885b5487789bcc1603041e0b
MAwais <mawais54013@outlook.com>
2020-01-06 21:14:58 -0800

    added menu

--- index.vr.js
+++ index.vr.js
@@ -5,17 +5,52 @@ import {
   Pano,
   Text,
   View,
+  StyleSheet
 } from 'react-vr';
 
-export default class WorldTour extends React.Component {
+class WorldTour extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showMenu: false
+    }
+  }
+
+  toggleMenu() {
+    this.setState({showMenu: !this.state.showMenu})
+  }
+
   render() {
     return (
       <View>
-        <Pano source={asset('360image5.jpg')}/>
-
+        <Pano source={asset('starry-sky.jpg')}/>
+        <View style={styles.menuButton} onEnter={() => this.toggleMenu()}>
+          <Text style={styles.menuButtonText}>{this.state.showMenu ? 'Close Menu' : 'Open Menu'}</Text>
+        </View>
       </View>
     );
   }
 };
+
+const styles = StyleSheet.create({
+  menuButton: {
+    backgroundColor: '#fff',
+    borderRadius: 0.25,
+    width: 0.5,
+    height: 0.5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 0.01,
+    transform: [
+      {translate: [-2, 0, -5]}
+    ]
+  },
+  menuButtonText: {
+    textAlign: 'center',
+    fontSize: 0.15,
+    color: '#000'
+  }
+})
 
 AppRegistry.registerComponent('WorldTour', () => WorldTour);
